e53921f5eb4576ff1b6e5ad4df3ff72d5b5e8fc5
Carmen Owen <carmenowen@carmenocomputer.home>
2018-05-13 22:33:23 -0400

    No JS yet

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var gameWords = [
+    "CYLON",
+    "RAPTOR",
+    "PEGASUS",
+    "CAPRICA",
+    "TOASTER",
+    "BALTAR",
+    "ATHENA",
+    "PASSACAGLIA",
+    "KOBOL",
+    "HANGAR",
+];
+
+
